d6a5ac2a9aae20ec833d134e5620f9c1af4365ff
Valoucoco <102355003+Valoucoco@users.noreply.github.com>
2022-11-03 11:56:47 +0100

    ajout des fonction name, location,title, etc, reste à faire la fonction pour les étoiles

--- src/components/FicheLogement.js
+++ src/components/FicheLogement.js
@@ -25,45 +25,63 @@ function FicheLogement({ title, content }) {
     });
     }, [id]);
 
+    ////////////////--  si l'id n'existe pas (titre inéxistant) alors go to 404  --////////////////
+
     if (logement.title === undefined) {
         return <Error />;
     }
 
-    const description = logement.description;
-    // const equipment = logement.equipments;
-    // const location = logement.location;
-    const logementEquipments = logement.equipments
-    const logementTags = logement.tags
+    ////////////////--    --////////////////
+    ////////////////--    --////////////////
 
-    const map_Logement = logementEquipments.map((l, index) => (
+    const logementDescription = logement.description;
+    const logementTitle = logement.title;
+    const logementEquipments = logement.equipments;
+    const logementLocation = logement.location
+    const logementName = logement.name
+    const logementTags = logement.tags;
+    const logementHostName = logement.host.name;
+    const logementHostPicture = logement.host.picture;
+    ////////////////--  map() des équipements  --////////////////
+    const map_LogementEquipement = logementEquipments.map((l, index) => (
         <p className="p_equipement_logement">{logement.equipments[index]}</p>
     ))
-
-    const map_Tags = logementTags.map((l, index) => (
+    ////////////////--  map() des tags  --////////////////
+    const map_logementTags = logementTags.map((l, index) => (
         <p className="p_equipement_logement" key={logement.tags[index]}>{logement.tags[index]}</p>
     ))
+
+    ////////////////--  return()  --////////////////
 
     return (
         <section>
             <article className="info_Logement_Container">
-                <h1>{logement.title}</h1>
-                <h2>{logement.location}</h2>
-                <div className="tags_Container">
-                    {map_Tags}
-                </div>
-                <div>
-                    <span>{logement.name}</span>
 
+                <h1>{logementTitle}</h1>
+
+                <h2>{logementLocation}</h2>
+
+                <div className="tags_Container">
+                    {map_logementTags}
                 </div>
+
+                <div>
+                    <h3>{logementName}</h3>
+                    <img className="logementHostPicture"
+                        src={logementHostPicture}
+                        alt="Portrait de l'annonceur"
+                    />
+                </div>
+
             </article>
             <div className="Accordion_Logement">
                 <AccordionLogement  
                     title="Description" 
-                    content={description}/>
+                    content={logementDescription}/>
 
                 <AccordionLogement  
                     title="Equipements" 
-                    content={<div className='equipments'>{map_Logement}</div>} />
+                    content={<div className='equipments'>{map_LogementEquipement}</div>} />
             </div>
         </section>
     );
